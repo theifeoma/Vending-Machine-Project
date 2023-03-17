12927e6964ecb756d40a949d1c400e3bdd54b51b
Ifeoma Okafor <if.comokafor@ymail.com>
2023-03-17 18:01:30 +0000

    five pence update

--- Coins/fivePence.cs
+++ Coins/fivePence.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 namespace VendingMachineProject
 {
-	public class fiftyPence
+	public class fivePence : Coin
 	{
-		public fiftyPence()
-		{
-		}
-	}
+        public fivePence(double name, int quantity, Coin nextValue)
+            : base(name, quantity, nextValue)
+        {
+            this.value = 0.05;
+            this.quantity = 20;
+        }
+
+        protected override void dispenseCoins(int count)
+        {
+            Console.WriteLine($"{count} x £0.10 coin");
+        }
+    }
 }
 
